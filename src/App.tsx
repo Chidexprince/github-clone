187389c5e02c29e5cea171142f51df6913045c34
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-07-16 14:50:48 +0100

    Setting up router for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import './App.css';
 import Login from './components/login';
+import { BrowserRouter, Route } from 'react-router-dom';
 import ResultPage from './components/result-page';
 import Header from './components/tags/header';
 import LandingPage from './components/landing-page';
 
+require('dotenv').config();
+
 function App() {
   return (
-    <div className="container-fluid">
+    <BrowserRouter>
+      <div className="container-fluid">
       <Login></Login>
       {/* <LandingPage></LandingPage>
       <Header></Header> */}
       {/* <ResultPage></ResultPage> */}
-    </div>
+      </div>
+
+      <Route exact path="/" component={Login} />
+          <Route path="/home" component={LandingPage} />
+          <Route path="/results" component={ResultPage} />
+    </BrowserRouter>
+    
   );
 }
 
